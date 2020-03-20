44a9e69167bc68721e60186fbe436c13a78fdf40
Megan L. Larsen <mlarsen@wlu.ca>
2020-03-20 00:36:38 -0400

    Update analysis_example.R
    added examples of plotting functions

--- analysis_example.R
+++ analysis_example.R
@@ -1,14 +1,28 @@
 
-# load the MAVEn dataset
-maven <- read_maven(maven_file = "./maven_output.csv")
+# load the raw MAVEn dataset
+maven_raw <- read_maven(maven_file = "./maven_output.csv", baseline = T)
+plot_maven_overview(maven_raw)
+
+
+# load the modified MAVEn dataset
+maven <- read_maven(maven_file = "./maven_output.csv", baseline = F)
 
 # assign a cycle number to the dataset
 maven <- cycle_number(maven)
 
+
+# Extract the metabolism data
 fly_metabolism <- extract_metabolism(maven)
 
-## generate summary table by cycle
-metabolism_summary <- summarize_metabolism(fly_metabolism, type = "by_cycle")
+# visualize the trend
+metablism_trend(fly_metabolism)
 
-## generate summary table by chamber
-metabolism_summary <- summarize_metabolism(fly_metabolism, type = "by_chamber")
+# generate summary table by cycle
+metabolism_summary_cycle <- summarize_metabolism(fly_metabolism, type = "by_cycle")
+
+# Metabolism diagnostic
+metabolism_diag(maven_raw, metabolism_summary_cycle)
+
+# generate summary table by chamber
+metabolism_summary_chamber <- summarize_metabolism(fly_metabolism, type = "by_chamber")
+
