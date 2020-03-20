17e385c21ffa1ed4eddc58c84710a652f2832da2
Megan L. Larsen <mlarsen@wlu.ca>
2020-03-19 22:41:44 -0400

    initial commit

---
+++ code/workspace.R
@@ -0,0 +1,93 @@
+code <- sapply(list.files(file.path(".", "code"), full.names = TRUE), source)
+
+maven <- read_maven()
+
+# assume that met measurements are > 0
+#This is the fly metabolism data
+
+
+cycle_starttime = maven$Seconds[maven$Chamber == 1][1]
+chamber_measure_duration = 120
+n_chambers = 16
+baseline_measurement_duration = 180
+cycle_duration = n_chambers * chamber_measure_duration
+
+#iterative time, multiply by number of cycles
+cycle_endtime = cycle_starttime + chamber_measure_duration * n_chambers
+
+test = maven
+test$cycle_number <- NA
+  
+for(i in 1:nrows(maven)){
+  test$cycle_number[test$Seconds[i] >= cycle_starttime & test$Seconds[i] < cycle_duration] <- i
+  cycle_starttime <- cycle_starttime + cycle_duration + baseline_measurement_duration
+}
+
+
+
+flychamber <- maven %>% 
+  select(Seconds:BP_kPa, c_FRC_mlmin:CO2_mlmin, FlyChamber1:FlyChamber16) %>%
+  pivot_longer(cols = FlyChamber1:FlyChamber16, names_to = "parameter", values_to = "result") %>%
+  arrange(Seconds)
+
+
+
+act <- maven %>% 
+  select(Seconds:BP_kPa, c_FRC_mlmin:CO2_mlmin, Act_1:Act_16) %>%
+  pivot_longer(cols = Act_1:Act_16, names_to = "parameter", values_to = "result") %>%
+  arrange(Seconds)
+
+met <- maven %>% 
+  select(Seconds:BP_kPa, c_FRC_mlmin:CO2_mlmin, CO2_mlminFly1:CO2_mlminFly16) %>%
+  pivot_longer(cols = CO2_mlminFly1:CO2_mlminFly16, names_to = "parameter", values_to = "result") %>%
+  arrange(Seconds)
+
+
+met$Seconds[met$Chamber == 1]
+
+measurement_duration = 120
+n_chambers = 16
+cycle_length = measurement_duration*n_chambers # number of rows in cycle
+n_cycles = nrow(met)/(n_chambers * measurement_duration)
+
+
+
+met$cycle_number <- NA
+
+cycle_start_time <- met$Seconds[1]
+cycle_end_time <- cycle_start_time + cycle_length
+
+for(i in 1:round(n_cycles)){
+  
+  met$cycle_number[met$Seconds >= cycle_start_time * i & met$Seconds < cycle_end_time * i] <- i
+  
+}
+
+baseline_time = 180
+
+for(i in 1:round(n_cycles)){
+  
+test <- met %>% 
+  filter(Seconds >= cycle_start_time & Seconds < cycle_end_time * i) %>% 
+  mutate(cycle_number = i) %>%
+  select(Seconds, cycle_number)
+
+cycle_start_time = cycle_start_time + cycle_end_time * i + baseline_time
+
+}
+
+test <- met %>%
+  mutate(cycle_number = ifelse(Seconds <= cycle_length+start_time, 1, 2)) %>% ## this is a brut force method. it will not work with more than 2 cycles
+  group_by(Chamber, cycle_number) %>%
+  mutate(measurement_number = Seconds - min(Seconds)+1)
+
+ggplot(data = test, aes(measurement_number, result, col = as.factor(cycle_number))) + 
+  geom_point() + facet_grid(~Chamber)
+
+test <- as.data.frame(test)
+
+summarize_metabolism <- function(df=""){
+  df %>%
+    group_by(Chamber, cycle_number) %>%
+    summarise(mean = mean(result), sd = sd(result))
+}
