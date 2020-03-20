e441c18ac5037f17be7b5c08584869f5360afacd
Megan L. Larsen <mlarsen@wlu.ca>
2020-03-20 02:59:31 -0400

    initial commit
    function to merge data files

---
+++ code/output.R
@@ -0,0 +1,5 @@
+create_data_table <- function(metabolism_summary_cycle, activity_summary_cycle){
+  metabolism_summary_cycle %>% 
+    left_join(activity_summary_cycle, by = c("Chamber","cycle"))
+}
+
