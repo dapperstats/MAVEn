bfdd3a99f3d8e4f251ed42d2baadbe7aec5d7a28
Megan L. Larsen <mlarsen@wlu.ca>
2020-03-26 02:13:54 -0400

    Update cycle_calculations.R
    cleaning up code; some rearrangement and documentation

--- R/cycle_calculations.R
+++ R/cycle_calculations.R
@@ -1,14 +1,54 @@
-calculate_cycles <- function(maven, n_chambers = 16, 
+# Function name will need to change; this function currently operates to detect
+# the first time Chamber 1 is read in the dataseries and allows for a variable
+# number of measurements per chamber. Waiting on James to confirm that this is
+# how he wants to proceed.
+# assign_cyclenumber
+assign_cyclenumber_at1_variable <- function(maven,
+    n_chambers = 16,
     chamber_measure_duration = 120) {
-    maven <- maven %>% arrange(Seconds)
-    cycle_duration <- n_chambers * chamber_measure_duration
-    n_cycles = nrow(maven)/(cycle_duration)
     
-    return(n_cycles)
+    df <-  maven %>% arrange(Seconds)
+    diff.list <- c()
+    
+    # Detect the difference between a row and use it as an indicator for 
+    #   the start of the next cycle
+    
+    for (i in 2:length(df$Chamber)) {
+        diff <- df$Chamber[i - 1] - df$Chamber[i]
+        diff.list <- append(diff.list, diff)
+    }
+    
+    # In some cases, the first value may not be detected because of where
+    #   the instrument starts its reading. 
+    
+    time.list <- unique(c(df[df$Chamber == 1, "Seconds"][1],
+        df[which(diff.list > 1) + 1, "Seconds"],
+        df[nrow(df), "Seconds"]))
+    
+    cycle_duration <- n_chambers * chamber_measure_duration
+    n_cycles = (nrow(df) - df$Seconds[time.list[1]]) / (cycle_duration)
+    message(paste("There were", n_cycles, "cycles detected in this dataset."))
+    df$cycle <- "NA"
+    
+    if((ceiling(n_cycles) - 1) > 1) {
+        for (i in 1:ceiling(n_cycles) - 1) {
+            df[df$Seconds >= time.list[i] &
+                    df$Seconds <= time.list[i + 1], "cycle"] <- i
+        }
+        
+    } else {
+        for (i in 1:ceiling(n_cycles)) {
+            df[df$Seconds >= time.list[i] &
+                    df$Seconds <= time.list[i + 1], "cycle"] <- i
+        }
+    }
+    
+    return(df)
 }
 
+
 # assign cycle numbers to the dataframe based on the first chamber measurement
-# this function might need to change depending on whether we need to start at 1
+# no flexibility in the number of measurements per chamber
 assign_cyclenumber <- function(maven, n_chambers = 16,
     chamber_measure_duration = 120) {
     
@@ -27,6 +67,9 @@ assign_cyclenumber <- function(maven, n_chambers = 16,
     df <- filter(df, !is.na(Seconds))
     return(df)
 }
+
+# This function operates with indexing and uses a fixed number of measurements. 
+# Unfortuntately, this assumption isn't always met
 
 assign_cyclenumber_at1 <- function(maven, n_chambers = 16,
     chamber_measure_duration = 120) {
@@ -57,49 +100,3 @@ assign_cyclenumber_at1 <- function(maven, n_chambers = 16,
     return(df)
 }
 
-assign_cyclenumber_at1_variable <- function(maven,
-    n_chambers = 16,
-    chamber_measure_duration = 120) {
-    
-    df <-  maven %>% arrange(Seconds)
-    diff.list <- c()
-    
-    #what rows, when difference between the two rows when its greater than 16-1
-    
-    for (i in 2:length(df$Chamber)) {
-        diff <- df$Chamber[i - 1] - df$Chamber[i]
-        diff.list <- append(diff.list, diff)
-    }
-    
-    index.list <- c(which(diff.list > 1), nrow(df))
-    
-    ## In some cases, the first value may not be detected
-    ##  this should capture all the necessary values
-    time.list <- unique(c(df[df$Chamber == 1, "Seconds"][1],
-        df[which(diff.list > 1) + 1, "Seconds"],
-        df[nrow(df), "Seconds"]))
-    
-    cycle_duration <- n_chambers * chamber_measure_duration
-    n_cycles = (nrow(df) - df$Seconds[index]) / (cycle_duration)
-    message(paste("There were", n_cycles, "cycles detected in this dataset."))
-    df$cycle <- "NA"
-    
-    if((ceiling(n_cycles) - 1) > 1) {
-        for (i in 1:ceiling(n_cycles) - 1) {
-            #df[index.list[i]+1:index.list[i+1], "cycle"] <- i
-            df[df$Seconds >= time.list[i] &
-                    df$Seconds <= time.list[i + 1], "cycle"] <- i
-            }
-        } else {
-        
-        for (i in 1:ceiling(n_cycles)) {
-            #df[index.list[i]+1:index.list[i+1], "cycle"] <- i
-            df[df$Seconds >= time.list[i] &
-                    df$Seconds <= time.list[i + 1], "cycle"] <- i
-            }
-        }
-    
-    print(unique(df$cycle))
-    
-    return(df)
-}
