8a820ed4334e2f938603b6ac5d3c19383c60f678
Megan L. Larsen <mlarsen@wlu.ca>
2020-03-27 01:38:10 -0400

    option to save output

--- R/output.R
+++ R/output.R
@@ -1,5 +1,5 @@
 maven_datatable <- function(metabolism_summary_cycle, activity_summary_cycle,
-  outdir = "output", out_filename = "", maven_experiment = "") {
+  outdir = "output", out_filename = NULL, maven_experiment = "") {
     
   table <- metabolism_summary_cycle %>% 
     left_join(activity_summary_cycle, by = c("Chamber", "cycle"))
@@ -7,7 +7,8 @@ maven_datatable <- function(metabolism_summary_cycle, activity_summary_cycle,
   outpath <- file.path(outdir, 
     out_filename = paste0(Sys.Date(),"_",maven_experiment, "_",
       out_filename, ".csv"))
-  
-  write.csv(table, file = outpath, row.names = F)
+  if(!is.null(out_filename)){  
+    write.csv(table, file = outpath, row.names = F)}
+
   return(table)
 }
