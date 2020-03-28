3db699776a9e9abcc660564bbe804b14a93505c1
Megan L. Larsen <mlarsen@wlu.ca>
2020-03-28 18:25:17 -0400

    Update evaluate.R
    added required bits for saving the datatable

--- R/evaluate.R
+++ R/evaluate.R
@@ -3,7 +3,9 @@
 evaluate_maven <- function(datadir = "data", outdir = "output",
                            maven_datafile = "./maven_output.csv", 
                            maven_experiment = "", 
-                           interval = 60, threshold = 0.01, 
+                           interval = 60, 
+                           activity_baseline = 0.01,
+                           activity_threshold = 1, 
                            figures = c("trend","diag","overview")){
   
   maven <- read_maven(maven_datafile = maven_datafile, baseline = F)
@@ -13,11 +15,14 @@ evaluate_maven <- function(datadir = "data", outdir = "output",
   metabolism_summary_cycle <- summarize_metabolism(fly_metabolism, type = "by_cycle")
   
   fly_activity <- extract_activity(maven.cycle, metabolism_summary_cycle, 
-                                   interval = interval, threshold = threshold)
+                                   interval = interval, 
+                                   activity_baseline = activity_baseline)
   
   
-  activity_summary_cycle <- summarize_activity(fly_activity, type = "by_cycle")
-  out <- maven_datatable(metabolism_summary_cycle, activity_summary_cycle) 
+  activity_summary_cycle <- summarize_activity(fly_activity, type = "by_cycle", 
+                                               activity_threshold = activity_threshold)
+  out <- maven_datatable(metabolism_summary_cycle, activity_summary_cycle, 
+                         maven_experiment = maven_experiment) 
   
   if ("overview" %in% figures) {
     maven_raw <- read_maven(maven_datafile = maven_datafile, baseline = T)
@@ -32,6 +37,10 @@ evaluate_maven <- function(datadir = "data", outdir = "output",
   if ("diag" %in% figures){
     metabolism_diag(maven_raw, metabolism_summary_cycle, 
                     maven_experiment = maven_experiment)
+    activity_diag(maven_raw, metabolism_summary_cycle, 
+                  activity_summary_cycle,
+                  maven_experiment = maven_experiment,
+                  interval = interval)
   }
   
   return(out)
